6da367f2f4a47ea4d03cd0987adf036078af3cdc
Milda <milda122603@gmail.com>
2023-10-16 22:34:08 +0800

    day2

---
+++ day2.java
@@ -0,0 +1,11 @@
+package Coding;
+
+public class day2 {
+    public static void main(String[] args) {
+        String nama = "Milda";
+        int umur = 19;
+        System.out.println("nama saya " + nama );
+        System.out.println("umur saya " + umur + " tahun ");
+    }
+    
+}

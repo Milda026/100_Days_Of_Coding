855a5bc3d14916e1627fc380886966836163cd72
Milda <milda122603@gmail.com>
2023-10-16 22:44:29 +0800

    day3

---
+++ day3.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Coding;
+
+/**
+ *
+ * @author Milda
+ */
+public class day3 {
+
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String nama = "MIlda";
+        String nim = "D0221318";
+        String kelas = "D";
+        String jurusan = "Teknik Informatika";
+        String alamat = "Lena";
+
+        // Mencetak biodata mahasiswa
+        System.out.println("Biodata Mahasiswa");
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("Jurusan: " + jurusan);
+        System.out.println("Alamat: " + alamat);
+    }
+
+}

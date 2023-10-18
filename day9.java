48b729e8e4b1e0bbccd791d0618cbc38fbf44a6c
Milda <milda122603@gmail.com>
2023-10-18 22:41:51 +0800

    Penjumlahan bilangan bulat menggunakan inputan

---
+++ day9.java
@@ -0,0 +1,19 @@
+package Coding;
+import java.util.Scanner;
+public class day9 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int bil1, bil2;
+        
+        System.out.print("Masukkan bilangan pertama: ");
+        bil1 = input.nextInt();
+        System.out.print("Masukkan bilangan kedua: ");
+        bil2 = input.nextInt();
+        
+        int hasil = bil1 + bil2;
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
+    
+

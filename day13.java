d923a4a9b37dacea652c6969ea5e2d39cc64c2f1
Milda <milda122603@gmail.com>
2023-10-21 21:00:26 +0800

    Mencetak angka dari 1 hingga 50 menggunakan perulangan for

---
+++ day13.java
@@ -0,0 +1,9 @@
+package Coding;
+
+public class day13 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i);
+        }
+    }
+}

5e63c267b39ca49cc4288e03ca53c7c061f15532
Milda <milda122603@gmail.com>
2023-10-23 20:24:43 +0800

    Deklarasi variabel sederhana

---
+++ day14.java
@@ -0,0 +1,19 @@
+package Coding;
+
+
+public class day14 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int angka;
+
+        // Inisialisasi variabel
+        angka = 10;
+
+        // Menampilkan nilai variabel
+        System.out.println("angka: " + angka);
+    }
+}
+
+
+
+ 

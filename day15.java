8632ceb5b61ea0f40ec1c19acf72213696f10364
Milda <milda122603@gmail.com>
2023-10-24 13:42:26 +0800

    mencetak angka 0 hingga 50 menggunakan perulangan

---
+++ day15.java
@@ -0,0 +1,10 @@
+package Coding;
+
+public class day15 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}

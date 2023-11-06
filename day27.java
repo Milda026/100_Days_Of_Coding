d77c7ddab913a2a294c414f51ab206809ebea7a7
Milda <milda122603@gmail.com>
2023-11-06 23:03:14 +0800

    mencetak bilangan dari 1 sampai batas yang ditentukan oleh pengguna

--- day27.java
+++ day27.java
@@ -9,9 +9,8 @@ public class day27 {
         System.out.println("Masukkan batas " );
         batas = sc.nextInt();
 
-        for (int i = 2; i <= batas; i += 2) {
+        for (int i = 1; i <= batas; i++) {
             System.out.println(i);
-        }
     }
-    
+    }
 }

881a0fa28f07e545775848f1b65418bdb76786d8
Milda <milda122603@gmail.com>
2023-11-11 23:35:21 +0800

    mencetak bilangan ganjil dari 1 sampai batas yang ditentukan dengan menghitung hasil pengurangan dari keseluruhan

---
+++ day33.java
@@ -0,0 +1,20 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class day33 {
+     public static void main(String[] args) {
+       Scanner m = new Scanner(System.in);
+        int batas ;
+        System.out.println("Masukkan batas " );
+        batas = m.nextInt();
+        
+        int total = 1;
+        for (int i = 1; i <= batas; i++) {
+            System.out.println(i);
+            total -=i;
+        }
+        System.out.println("Hasil : "+total);
+    } 
+    
+}

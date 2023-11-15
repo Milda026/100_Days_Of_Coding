585fd6209c4ae662c40eb194d0c5450ca10adbbc
Milda <milda122603@gmail.com>
2023-11-16 00:36:24 +0800

    menciptakan sebuah loop tak terbatas (infinite loop)

---
+++ day36.java
@@ -0,0 +1,12 @@
+package Coding;
+
+public class day36 {
+    public static void main(String[] args) {
+        for (int i = 0; true; i++) {
+            if (i == 100) {
+                break;
+            }
+            System.out.println("Ini adalah infinite loop");
+        }
+    }
+}
